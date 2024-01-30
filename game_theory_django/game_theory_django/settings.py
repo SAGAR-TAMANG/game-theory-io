b7abc8dfed7160825ac423aadd75e82b5ee6a6e1
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-01-30 16:11:17 +0530

    latest-

--- game_theory_django/game_theory_django/settings.py
+++ game_theory_django/game_theory_django/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os.path
 
 from pathlib import Path
 
@@ -127,6 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_DIR =  [   
+    os.path.join(BASE_DIR, 'home/static'),
+]
 
 STATIC_URL = 'static/'
 
