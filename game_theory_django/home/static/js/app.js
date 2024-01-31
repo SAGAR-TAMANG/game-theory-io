b524219322d2c5860d4b4145fdf7728dc6d290de
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-01-31 21:53:39 +0530

    added tutorial and play pages

--- game_theory_django/home/static/js/app.js
+++ game_theory_django/home/static/js/app.js
@@ -1,16 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var count = 0; // Move count outside the event listener to keep track globally
+var currentFileName = window.location.pathname.split('/').pop();
 
-  var video = document.querySelector('.video');
+if (currentFileName == '') {
+  document.addEventListener('DOMContentLoaded', function () {
+    var count = 0;
 
-  video.addEventListener('ended', function() {
-    if (count < 1) {
-      video.src = "static/video/repeat.mp4";
-      count += 1;
-      console.log("VIDEO ENDED");
-      video.loop = true;
-    }
+    var video = document.querySelector('.video');
+
+    video.addEventListener('ended', function () {
+      if (count < 1) {
+        video.src = "static/video/repeat.mp4";
+        count += 1;
+        console.log("VIDEO ENDED");
+        video.loop = true;
+      }
+    });
+    playButton = document.querySelector('.play_button');
+    tutorialCard = document.querySelector('.tutorial_card');
+
+    playButton.addEventListener('click', function () {
+      console.log("CLICKED");
+      tutorialCard.style.transform = "translate(50%, -110%)";
+    });
   });
-});
+}
 
-console.log("LOADED")
+if (currentFileName == 'play') {
+}
+
+
+console.log("LOADED");
--- game_theory_django/home/views.py
+++ game_theory_django/home/views.py
@@ -6,6 +6,16 @@ def index(request):
   return render(request, 'index.html')
 
 def play(request):
-  choice = random.choice(['cooperate.mp4', 'defect.mp4'])
-  print("Choice: ", choice)
-  return render(request, 'play.html', {'choice':choice})
+    if 'choice_list' not in request.session:
+        # If choice_list is not in the session, generate and store it
+        choice_list = generate_choice_list()
+        request.session['choice_list'] = choice_list
+    else:
+        # Retrieve the existing choice_list from the session
+        choice_list = request.session['choice_list']
+
+    return render(request, 'play.html', {'choice_list': choice_list})
+
+def generate_choice_list():
+    # Generate a random sequence of 10 choices
+    return [random.choice(['cooperate', 'defect']) for _ in range(10)]
